985ae754aafbf44f55580846559b1342e14341d9
Stefan Ober <StefanO@ppedv.de>
2024-01-23 15:14:48 +0100

    Skript mit Verbose ausgabe

--- Skriptbeispiel/Get-Eventlog.ps1
+++ Skriptbeispiel/Get-Eventlog.ps1
@@ -1,4 +1,5 @@
-﻿param(
+﻿[cmdletBinding()]
+param(
 [Parameter(Mandatory=$true)]
 [int]$EventId,
 
@@ -6,6 +7,8 @@
 
 [string]$Computername = "localhost"
 )
+Write-Verbose -Message "Ich gebe eine zusätzliche Ausgabe aus wenn das Skript mit -verbose gestartet wird. Anforderung ist hierbei das [cmdletBinding()]"
+Write-Verbose -Message "Vor der Abfrage, $EventId , $Newest , $Computername"
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
 
