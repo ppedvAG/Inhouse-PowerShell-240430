cf357eebbad5d23c2d20090a0af32644888f61c9
Stefan Ober <StefanO@ppedv.de>
2024-01-23 15:10:39 +0100

    Parameter2

--- Skriptbeispiel/Get-Eventlog.ps1
+++ Skriptbeispiel/Get-Eventlog.ps1
@@ -1,7 +1,10 @@
 ﻿param(
-$EventId,
-$Newest,
-$Computername
+[Parameter(Mandatory=$true)]
+[int]$EventId,
+
+[int]$Newest = 5,
+
+[string]$Computername = "localhost"
 )
 Get-EventLog -LogName Security -ComputerName $Computername | Where-Object EventId -eq $EventId | Select-Object -First $Newest
 
